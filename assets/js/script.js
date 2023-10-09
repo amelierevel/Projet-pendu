c58d5f68c8635556bf5dedad789c87f1b1da9c98
amelierevel <amelie.revel20@yahoo.fr>
2023-10-09 11:53:18 +0200

    js commenté, reste rwd

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,9 +32,6 @@ document.getElementById('startGame').addEventListener('click', startGameFunction
 
 // --> On déclare la fonction "startGameFunction" qui initialise la 1ère partie (masque le 1er écran, affiche le 2e écran et définit le mot à trouver)
 function startGameFunction() {
-    // -> On incrémente le compteur de partie
-    gamesCount++;
-
     // -> On masque le 1er écran et on affiche le 2e écran
     document.getElementById('container-start').style.display = 'none';
     document.getElementById('container-game').style.display = 'flex';
@@ -44,9 +41,106 @@ function startGameFunction() {
 }
 
 
+//----------Au clic sur le btn "Proposer cette lettre" on appelle la fonction "checkLetterFunction"
+letterGuessBtn.addEventListener('click', checkLetterFunction);
+
+// --> On déclare la fonction "checkLetterFunction" qui récupère la valeur saisie par l'utilisateur et vérifie qu'elle correspond bien à une lettre et que cette lettre est présente ou non dans le mot à trouver (si oui on affiche la lettre si non on incrémente le pendu)
+function checkLetterFunction() {
+    // -> On vide le message d'erreur
+    document.getElementById('errorMessage').innerHTML = '';
+    
+    // -> On déclare une variable "userLetter" qui stocke la valeur saisie par l'utilisateur
+    let userLetter = document.getElementById('letterGuess').value;
+
+    // -> On vérifie que la valeur saisie par l'utilisateur est correcte (1 lettre)
+    //si "userLetter" est un nombre (n'est pas (!) pas un nombre(isNaN)) ou si la longueur de "userLetter" n'est pas égal à 1 alors...
+    if (!isNaN(userLetter) || userLetter.length != 1) {
+        //on affiche un message d'erreur et on réinitialise le champ de saisie (on définit une valeur vide dans l'input)
+        document.getElementById('errorMessage').innerHTML = 'Veuillez entrer une lettre';
+        document.getElementById('letterGuess').value = '';
+    //sinon... (dans le cas où il n'y a pas d'erreur)
+    } else {
+        //on ajoute la lettre de l'utilisateur au tableau "lettersTry" et on affiche cette lettre dans l'élément html prévu pour que l'utilisateur puisse voir ses tentatives
+        lettersTry.push(userLetter);
+        document.getElementById('lettersTry').innerHTML = document.getElementById('lettersTry').innerHTML + userLetter + ' - ';
+
+        //on réinitialise le champ de saisie (on définit une valeur vide dans l'input)
+        document.getElementById('letterGuess').value = '';
+
+        //on déclare et initialise les variables qui seront utiles pour la suite :
+        // - letterFound -> variable booléenne, fausse quand la lettre n'est pas trouvée et vraie quand la lettre est trouvée)
+        // - wordWrite -> variable qui contiendra le mot construit au fur et à mesure par l'utilisateur
+        // - idLetter -> variable qui contiendra le nom de l'id des lettres 
+        let letterFound = false;
+        let wordWrite = '';
+        let idLetter;
+
+        // -> On boucle sur le mot pour comparer la lettre de l'utilisateur avec chaque lettre du mot à trouver une par une
+        //pour un nombre "a" allant de 0 à la longueur du mot à trouver avec un pas de 1...
+        for (a = 0; a < wordToFind.length; a++) {
+            //on attribut la valeur de l'id à "idLetter" 
+            idLetter = 'letter' + (a + 1);
+            //si la lettre de l'utilisateur est la même que la lettre "a" du mot à trouver (au 1er tour on compare la 1ère lettre du mot, au 2ème tour on compare la 2ème lettre du mot, etc.) alors...
+            if (userLetter == wordToFind[a]) {
+                //on affiche la lettre dans l'élément html dont l'id est "idLetter" et on définit que la lettre a été trouvé 
+                document.getElementById(idLetter).innerHTML = wordToFind[a];
+                letterFound = true;
+            }
+            //on attribue la valeur de "wordWrite" + la valeur de chaque élément html "lettre" pour construire le mot de l'utilisateur
+            wordWrite = wordWrite + document.getElementById(idLetter).innerHTML;
+            console.log(wordWrite);
+        }
+        
+        // -> Si la lettre n'est pas trouvée alors on incrémente le compteur d'erreur
+        if (!letterFound) {
+            tryCount++;
+            //si le compteur d'erreur est bien compris entre 0 et 10 (10 erreurs étant le max pour afficher le pendu complet) alors on affiche l'image du pendu correspondant au nombre d'erreurs de l'utilisateur
+            if (tryCount > 0 && tryCount <= 10) {
+                document.getElementById('hanged').src = 'assets/img/pendu' + tryCount + '.svg';
+            }
+
+            //Si le compteur d'erreur atteint 10 alors la partie est perdue
+            if (tryCount == 10) {
+                //on affiche les défaites de l'utilisateur et victoires de l'ordinateur
+                document.getElementById('userDefeatsCountShown').innerHTML = gamesCount - userVictoriesCount;
+                document.getElementById('computerVictoriesCountShown').innerHTML = gamesCount - userVictoriesCount;
+                //on affiche le résultat "Perdu", on masque le pendu et on affiche le bouton rejouer
+                document.getElementById('announcement-result-game').innerHTML = 'Perdu !';
+                document.getElementById('hanged').style.display = 'none';
+                document.getElementById('play-new-game').style.display = 'block';
+                //on supprime la class hanged-position pour permettre au contenu d'être centré (cf. css)
+                document.getElementById('hanged-space').classList.remove('hanged-position');
+            }
+        }
+
+        // -> Si le mot reconstruit par l'utilisateur correspond au mot à trouver alors...
+        if (wordWrite == wordToFind) {
+            //on incrémente le nombre de victoires de l'utilisateur
+            userVictoriesCount++;
+            //on affiche les victoires de l'utilisateur et défaites de l'ordinateur
+            document.getElementById('userVictoriesCountShown').innerHTML = userVictoriesCount;
+            document.getElementById('computerDefeatsCountShown').innerHTML = userVictoriesCount;
+            //on affiche le résultat "Gagné", on masque le pendu et on affiche le bouton rejouer
+            document.getElementById('announcement-result-game').innerHTML = 'Gagné !!';
+            document.getElementById('hanged').style.display = 'none';
+            document.getElementById('play-new-game').style.display = 'block';
+            //on supprime la class hanged-position pour permettre au contenu d'être centré (cf. css)
+            document.getElementById('hanged-space').classList.remove('hanged-position');
+        }
+    }
+}
+
+
+//----------Au clic sur le btn "Rejouer" on appelle la fonction "resetGameFunction" 
+document.getElementById('play-new-game').addEventListener('click', resetGameFunction);
+
+
 //----------Fonctions qui seront appelées à divers moments
 // -> On déclare la fonction "findWordFunction" qui définit le mot à trouver 
 function findWordFunction(){
+    // -> On incrémente le compteur de partie
+    gamesCount++;
+
     // -> On affecte à "wordToFind" un mot choisit aléatoirement dans le tableau de mots. 
     //la fonction "Math.random()..." retourne une valeur aléatoire entre 0 et la longueur du tableau. "Math.round()" permet d'arrondir cette valeur à l'entier le plus proche.
     wordToFind = wordsList[Math.round(Math.random() * (wordsList.length - 1))];
@@ -61,78 +155,7 @@ function findWordFunction(){
 }
 
 
-
-
-
-
-
-//Au clic sur le btn "Proposer cette lettre" 
-// -> Récupération de la lettre proposée par l'utilisateur (userLetter)
-// -> Parcourir tous les caractères du mot wordToFind, si userLetter est présente dans le mot alors l'afficher sinon incrémenter le compteur de tentatives et afficher un élément au pendu
-// -> Si le compteur de tentatives atteint la valeur max (pendu complet), alors afficher "Perdu" et incrémenter le compteur de défaites
-// -> Si le mot est complètement reconstitué, alors afficher "Gagné" et incrémenter le compteur de victoires
-letterGuessBtn.addEventListener('click', function () {
-    document.getElementById('errorMessage').innerHTML = '';
-    let userLetter = document.getElementById('letterGuess').value;
-    if (!isNaN(userLetter) || userLetter.length != 1) {
-        document.getElementById('errorMessage').innerHTML = 'Veuillez entrer une lettre';
-        document.getElementById('letterGuess').value = '';
-    } else {
-        lettersTry.push(userLetter);
-        document.getElementById('lettersTry').innerHTML = document.getElementById('lettersTry').innerHTML + userLetter + ' - ';
-
-        document.getElementById('letterGuess').value = '';
-
-        let letterFound = false;
-        let wordWrite = '';
-        let idLetter;
-
-        for (a = 0; a < wordToFind.length; a++) {
-            idLetter = 'letter' + (a + 1);
-            if (userLetter == wordToFind[a]) {
-                document.getElementById(idLetter).innerHTML = wordToFind[a];
-                letterFound = true;
-            }
-            wordWrite = wordWrite + document.getElementById(idLetter).innerHTML;
-        }
-        
-        if (!letterFound) {
-            tryCount++;
-            console.log(tryCount);
-            if (tryCount > 0 && tryCount <= 10) {
-                document.getElementById('hanged').src = 'assets/img/pendu' + tryCount + '.svg';
-            }
-        }
-        if (tryCount == 4) {
-            document.getElementById('userDefeatsCountShown').innerHTML = gamesCount - userVictoriesCount;
-            document.getElementById('computerVictoriesCountShown').innerHTML = gamesCount - userVictoriesCount;
-           
-            document.getElementById('announcement-result-game').innerHTML = 'Perdu !';
-            document.getElementById('hanged').style.display = 'none';
-            document.getElementById('play-new-game').style.display = 'block';
-            //Suppression de la class hanged-position pour permettre au contenu d'être centré
-            document.getElementById('hanged-space').classList.remove('hanged-position');
-        }
-
-        if (wordWrite == wordToFind) {
-            userVictoriesCount++;
-            document.getElementById('userVictoriesCountShown').innerHTML = userVictoriesCount;
-            document.getElementById('computerDefeatsCountShown').innerHTML = userVictoriesCount;
-
-            document.getElementById('announcement-result-game').innerHTML = 'Gagné !!';
-            document.getElementById('hanged').style.display = 'none';
-            document.getElementById('play-new-game').style.display = 'block';
-            //Suppression de la class hanged-position pour permettre au contenu d'être centré
-            document.getElementById('hanged-space').classList.remove('hanged-position');
-        }
-    }
-});
-
-
-//Appel de la fonction resetGame au clic sur le btn "Rejouer"
-document.getElementById('play-new-game').addEventListener('click', resetGameFunction);
-
-//Fonction resetGameFunction qui réinitialise le jeu et relance un nouveau mot à trouver 
+// -> On déclare la fonction "resetGameFunction" qui réinitialise le jeu et relance un nouveau mot à trouver 
 function resetGameFunction(){
     document.getElementById('announcement-result-game').innerHTML = '';
     document.getElementById('play-new-game').style.display = 'none';
